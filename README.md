4f3f4203562488fc3b6bc1a1a4d4ca0a326448e1
Arthur Zheng <arthurzheng150@gmail.com>
2026-05-02 13:20:51 +0800

    init

---
+++ README.md
@@ -0,0 +1,14 @@
+# NRF24l01
+
+![Version](https://img.shields.io/github/v/tag/openblock-plugin/nrf24l01?label=version)
+![License](https://img.shields.io/badge/license-MIT-green.svg)
+![Type](https://img.shields.io/badge/type-extension-orange.svg)
+![Tags](https://img.shields.io/badge/tags-communication-lightgrey.svg)
+
+2.4G RF transceiver module.
+
+## Documentation
+
+For detailed usage instructions, please visit:
+
+**[OpenBlock Wiki](https://wiki.openblock.cc)**
---
+++ src/translations.js
@@ -0,0 +1,95 @@
+/* eslint-disable quote-props */
+/* eslint-disable max-len */
+/**
+ * Translation file for this resouce.
+ *
+ * IMPORTANT:
+ * - The "en" (English) section is automatically generated from source files.
+ *   Do NOT modify the "en" section manually.
+ * - Other language sections (e.g., "zh-cn", "zh-tw", "ja", etc.) should be
+ *   manually translated by you.
+ * - When you run the extraction script again, only the "en" section will be
+ *   updated. Your manual translations in other languages will be preserved.
+ *
+ * Structure:
+ * - interface: translations for name/description (used by GUI formatMessage)
+ * - extensions: translations for extension blocks (used by VM formatMessage)
+ * - blocks: translations for Blockly blocks (used by Blockly.Msg)
+ */
+
+export default {
+    'interface': {
+        'en': {
+            'nrf24l01.description': '2.4G RF transceiver module.'
+        },
+        'ru': {
+            'nrf24l01.description': '2.4G RF transceiver module.'
+        },
+        'zh-cn': {
+            'nrf24l01.description': '2.4G RF transceiver module.'
+        },
+        'zh-tw': {
+            'nrf24l01.description': '2.4G RF transceiver module.'
+        }
+    },
+    'extensions': {
+        'en': {},
+        'ru': {},
+        'zh-cn': {},
+        'zh-tw': {}
+    },
+    'blocks': {
+        'en': {
+            'NRF24L01_CATEGORY': 'NRF24l01',
+            'NRF24L01_INIT': 'init nrf24l01 pin CE %1 CSN %2 id %3',
+            'NRF24L01_SENDNUMBER': 'nrf24l01 send number %1 to id %2',
+            'NRF24L01_SENDSTRING': 'nrf24l01 send string %1 to id %2',
+            'NRF24L01_SENDVALUE': 'nrf24l01 send value %1 = %2 to id %3',
+            'NRF24L01_HASDATA': 'nrf24l01 received data?',
+            'NRF24L01_READDATA': 'nrf24l01 read data',
+            'NRF24L01_PARSENUMBER': 'nrf24l01 parse number',
+            'NRF24L01_PARSESTRING': 'nrf24l01 parse string',
+            'NRF24L01_VALUEAVAILABLE': 'nrf24l01 value %1 available?',
+            'NRF24L01_PARSEVALUE': 'nrf24l01 parse value %1'
+        },
+        'ru': {
+            'NRF24L01_CATEGORY': 'NRF24l01',
+            'NRF24L01_INIT': 'init nrf24l01 pin CE %1 CSN %2 id %3',
+            'NRF24L01_SENDNUMBER': 'nrf24l01 send number %1 to id %2',
+            'NRF24L01_SENDSTRING': 'nrf24l01 send string %1 to id %2',
+            'NRF24L01_SENDVALUE': 'nrf24l01 send value %1 = %2 to id %3',
+            'NRF24L01_HASDATA': 'nrf24l01 received data?',
+            'NRF24L01_READDATA': 'nrf24l01 read data',
+            'NRF24L01_PARSENUMBER': 'nrf24l01 parse number',
+            'NRF24L01_PARSESTRING': 'nrf24l01 parse string',
+            'NRF24L01_VALUEAVAILABLE': 'nrf24l01 value %1 available?',
+            'NRF24L01_PARSEVALUE': 'nrf24l01 parse value %1'
+        },
+        'zh-cn': {
+            'NRF24L01_CATEGORY': 'NRF24l01',
+            'NRF24L01_INIT': 'init nrf24l01 pin CE %1 CSN %2 id %3',
+            'NRF24L01_SENDNUMBER': 'nrf24l01 send number %1 to id %2',
+            'NRF24L01_SENDSTRING': 'nrf24l01 send string %1 to id %2',
+            'NRF24L01_SENDVALUE': 'nrf24l01 send value %1 = %2 to id %3',
+            'NRF24L01_HASDATA': 'nrf24l01 received data?',
+            'NRF24L01_READDATA': 'nrf24l01 read data',
+            'NRF24L01_PARSENUMBER': 'nrf24l01 parse number',
+            'NRF24L01_PARSESTRING': 'nrf24l01 parse string',
+            'NRF24L01_VALUEAVAILABLE': 'nrf24l01 value %1 available?',
+            'NRF24L01_PARSEVALUE': 'nrf24l01 parse value %1'
+        },
+        'zh-tw': {
+            'NRF24L01_CATEGORY': 'NRF24l01',
+            'NRF24L01_INIT': 'init nrf24l01 pin CE %1 CSN %2 id %3',
+            'NRF24L01_SENDNUMBER': 'nrf24l01 send number %1 to id %2',
+            'NRF24L01_SENDSTRING': 'nrf24l01 send string %1 to id %2',
+            'NRF24L01_SENDVALUE': 'nrf24l01 send value %1 = %2 to id %3',
+            'NRF24L01_HASDATA': 'nrf24l01 received data?',
+            'NRF24L01_READDATA': 'nrf24l01 read data',
+            'NRF24L01_PARSENUMBER': 'nrf24l01 parse number',
+            'NRF24L01_PARSESTRING': 'nrf24l01 parse string',
+            'NRF24L01_VALUEAVAILABLE': 'nrf24l01 value %1 available?',
+            'NRF24L01_PARSEVALUE': 'nrf24l01 parse value %1'
+        }
+    }
+};
